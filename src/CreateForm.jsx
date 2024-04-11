26bc3d9136f0f0966d109f7d978dce5455862a1b
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-04-11 18:59:15 +0300

    Update CreateForm component

---
+++ src/CreateForm.jsx
@@ -0,0 +1,18 @@
+export function CreateForm() {
+  return (
+    <form className="d-flex justify-content-center align-items-center mb-4 w-100">
+      <div className="form-floating flex-fill">
+        <input type="text" id="form2" className="form-control" />
+        <label htmlFor="form2">New task...</label>
+      </div>
+      <button
+        type="submit"
+        data-mdb-button-init
+        data-mdb-ripple-init
+        className="btn btn-info ms-2"
+      >
+        Add
+      </button>
+    </form>
+  );
+}
