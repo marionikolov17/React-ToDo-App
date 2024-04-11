cf9740a283cb215ee684eb2fff596a4821b2794a
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-04-11 19:00:54 +0300

    Import TabsNavigation component in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./style.css";
 import { TodoItem } from "./TodoItem";
 import { CreateForm } from "./CreateForm";
+import TabsNavigation from "./TabsNavigation";
 
 function App() {
   return (
@@ -14,51 +15,7 @@ function App() {
                 <div className="card-body p-5">
                   <CreateForm />
 
-                  <ul
-                    className="nav nav-tabs mb-4 pb-2"
-                    id="ex1"
-                    role="tablist"
-                  >
-                    <li className="nav-item" role="presentation">
-                      <a
-                        className="nav-link active"
-                        id="ex1-tab-1"
-                        data-mdb-tab-init
-                        href="#ex1-tabs-1"
-                        role="tab"
-                        aria-controls="ex1-tabs-1"
-                        aria-selected="true"
-                      >
-                        All
-                      </a>
-                    </li>
-                    <li className="nav-item" role="presentation">
-                      <a
-                        className="nav-link"
-                        id="ex1-tab-2"
-                        data-mdb-tab-init
-                        href="#ex1-tabs-2"
-                        role="tab"
-                        aria-controls="ex1-tabs-2"
-                        aria-selected="false"
-                      >
-                        Active
-                      </a>
-                    </li>
-                    <li className="nav-item" role="presentation">
-                      <a
-                        className="nav-link"
-                        id="ex1-tab-3"
-                        data-mdb-tab-init
-                        href="#ex1-tabs-3"
-                        role="tab"
-                        aria-controls="ex1-tabs-3"
-                        aria-selected="false"
-                      >
-                        Completed
-                      </a>
-                    </li>
-                  </ul>
+                  <TabsNavigation />
                   <div className="tab-content" id="ex1-content">
                     <div
                       className="tab-pane fade show active"
